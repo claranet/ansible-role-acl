9073d110a31d55dd6c7d57bdf0bed245fc58299c
Delibes Bechir BKWEDOU-NGAMENI <b.bkwedou-ngameni@fr.clara.net>
2022-10-14 21:24:25 +0200

    rename private variables

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,6 +2,7 @@
 
 import os
 import stat
+
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -9,6 +10,21 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-def test_shell(host):
-    command = host.run("sh --version")
-    assert command.rc == 0
+def test_acl_is_installed(host):
+    _acl_package = host.package("acl")
+    assert _acl_package.is_installed
+
+
+def test_file_exist_and_perm_change(host):
+    """Validate acl file."""
+    _file_controlled_by_acl = host.run("sudo getfacl /etc/acl_file | grep user01")
+    assert _file_controlled_by_acl.succeeded
+
+
+def test_directory_exist_and_mode_change(host):
+    """Validate acl directory."""
+    _directory_controlled_by_acl = host.file("/etc/acl_directory")
+
+    assert _directory_controlled_by_acl.exists
+    assert _directory_controlled_by_acl.is_directory
+    assert _directory_controlled_by_acl.mode == 0o775
